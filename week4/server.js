5592b2ca7bf7afe4d10cf1b48c6cb73de57f8567
s225231701@deakin.edu.au <s225231701@deakin.edu.au>
2025-08-07 14:47:42 +1000

    week 4

--- week4/server.js
+++ week4/server.js
@@ -1,49 +1,60 @@
-var express = require("express")
-var app = express()
-app.use(express.static(__dirname+'/public'))
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const cardList = [
-{
-title: "Kitten 2",
-image: "images/kitten-2.jpg",
-link: "About Kitten 2",
-desciption: "Demo desciption about kitten 2"
-},
-{
-title: "Kitten 3",
-image: "images/kitten-3.jpg",
-link: "About Kitten 3",
-desciption: "Demo desciption about kitten 3"
-}
-]
-app.get('/api/projects',(req,res) => {
-res.json({statusCode: 200, data: cardList, message:"Success"})
-})
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpg",
+        link: "About Kitten 2",
+        desciption: "Demo desciption about kitten 2",
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        desciption: "Demo desciption about kitten 3",
+    },
+];
+app.get("/api/projects", (req, res) => {
+    res.json({ statusCode: 200, data: cardList, message: "Success" });
+});
 var port = process.env.port || 3000;
-app.listen(port,()=>{
-console.log("App listening to: "+port)
-})
-const getProjects = () => {
-$.get('/api/projects',(response) => {
-if(response.statusCode==200){
-addCards(response.data);
-}
-})
-}
-$(document).ready(function(){
-$('.materialboxed').materialbox();
-$('#formSubmit').click(()=>{
-submitForm();
-})
-getProjects();
-$('.modal').modal();
+app.listen(port, () => {
+    console.log("App listening to: " + port);
 });
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myprojectDB', {
-useNewUrlParser: true,
-useUnifiedTopology: true,
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/myprojectDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
-mongoose.connection.on('connected', () => {
-console.log('Connected to MongoDB!');
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB!");
 });
+
+const ProjectSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    link: String,
+    description: String,
+});
+const Project = mongoose.model("Project", ProjectSchema);
+
+app.get("/api/projects", async (req, res) => {
+    const projects = await Project.find({});
+    res.json({ statusCode: 200, data: projects, message: "Success" });
+});
+
+app.listen(port, () => {
+    console.log("App listening on port " + port);
+});
+
+const sampleProject = new Project({
+    title: "Kitten 4",
+    image: "images/kitten-4.jpg",
+    link: "About Kitten 4",
+    description: "Demo description about kitten 4"
+});
+sampleProject.save().then(() => console.log("Sample project saved!"));
